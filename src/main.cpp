1831cb381fab4dca1f5da9fc4c58476605315dde
Aaronkumar Ehambram <ehambram@rts.uni-hannover.de>
2023-07-06 16:52:40 +0200

    test the force interfaces

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "InvertedPendulum.hpp"
 #include <math.h>
+#include <iostream>
 
 int main()
 {
@@ -10,15 +11,22 @@ int main()
   std::vector<double> x0(4);
   x0[0] = -0.0;     // x
   x0[1] = 0.0;      // x'
-  x0[2] = M_PI/2;   // theta
+  x0[2] = 0;   // theta
   x0[3] = 0;        // theta'
   model.initialize(x0);
 
   // start simulation in seperate thread
-  double dt = 0.001;
-  std::thread simulation_thread(&InvertedPendulum::simulate, model, dt); 
+  double dt = 0.03;
+  std::thread simulation_thread(&InvertedPendulum::simulate, &model, dt); 
 
-  // TODO: test the force interface by setting forces in while loop! 
+  // test the force interface by setting forces in while loop! 
+  while(true)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(850));
+    model.setForce(0.3);
+    std::this_thread::sleep_for(std::chrono::milliseconds(850)); 
+    model.setForce(-0.3);
+  }
 
   // script comes to an end, wait for threads to be finished
   simulation_thread.join();
